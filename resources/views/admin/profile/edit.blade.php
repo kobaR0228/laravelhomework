6bc8a5ea5353fcee1f2d740b7e6a8e7c04fe5923
EC2 Default User <ec2-user@ip-172-31-34-234.ap-northeast-1.compute.internal>
2019-06-23 09:46:01 +0000

    fifth commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>これは編集用</h1>
-    
-</body>
-</html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロファイル編集作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
